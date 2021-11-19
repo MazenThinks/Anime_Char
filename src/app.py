985f4980664e815f410e75050fd3b89c487ddb53
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-19 21:44:39 +0200

    Fav char - Onii chan

---
+++ src/app.py
@@ -0,0 +1,14 @@
+
+
+y = "(yes .. {} kun is here! <3)"
+
+mylist = ("Ereh" , "Lighto" , "L" , "Armin" , "Levi")
+
+x = input("Enter your fav anime char name : ")
+if x in mylist:
+    print(y.format(x))
+else:
+    print("Sorry, hes not my fav too :( .. getoutafhere")
+
+
+ 
